d6ba4f72988f1da13b55f7214f1746a4112aa231
naveen <naveenkumar9707@gmail.com>
2024-10-12 13:02:32 -0500

    vpc

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 module "vpc" {
   source = "git::https://github.com/Naveen2015/tf-module-vpc.git"
+  env = var.env
   for_each = var.vpc
 
-  cidr_block= each.value["cidr_block"]
+  cidr_block = each.value["cidr_block"]
+  tags = local.tags
 }
