727ff9c97181e5385cc1fa1a32ba75c4e50f1967
naveen <naveenkumar9707@gmail.com>
2024-10-13 11:34:37 -0500

    asg

--- main.tf
+++ main.tf
@@ -11,3 +11,27 @@ module "vpc" {
 output "kruthika" {
   value = module.vpc
 }
+
+
+module "app" {
+  source = "git::https://github.com/Naveen2015/tf-module-app.git"
+  for_each = var.app
+  instance_type = each.value["instance_type"]
+  bastion_cidr = var.bastion_cidr
+  desired_capacity = each.value["desired_capacity"]
+  max_size = each.value["max_size"]
+  min_size = each.value["min_size"]
+
+  subnet_ids = lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["subnet_name"],null),"subnet_ids",null)
+  name = each.value["name"]
+  env = var.env
+  vpc_id = lookup(lookup(module.vpc,"main",null),"vpc_id",null)
+  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["allow_app_cidr"],null),"subnet_cidrs",null)
+}
+
+
+
+
+
+
+#element(lookup(lookup(lookup(lookup(module.vpc,"main",null),"subnets",null),each.value["subnet_name"],null),"subnet_ids",null),0)
