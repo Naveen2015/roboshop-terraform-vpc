244658a5e8c30047b050071d981091e1d45400d6
naveen <naveenkumar9707@gmail.com>
2024-10-27 14:22:54 -0500

    paymnet

--- main.tf
+++ main.tf
@@ -99,6 +99,7 @@ module "rabbitmq" {
   allow_db_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
 
   kms_arn        = var.kms_arn
+  domain_id = var.domain_id
 
   instance_type = each.value["instance_type"]
   bastion_cidr= var.bastion_cidr
